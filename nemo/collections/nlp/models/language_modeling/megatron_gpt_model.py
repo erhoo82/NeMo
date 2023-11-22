2b43e70ce8d65699719ae26f9fdd1f1741f2eba1
Chen Cui <chcui@nvidia.com>
2023-11-22 13:43:36 -0800

    support packed dataset
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    support packed dataset
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    [Codec] Finite scalar quantizer (#7886)
    
    * Finite scalar quantizer
    
    Signed-off-by: Ante Jukić <ajukic@nvidia.com>
    
    * Updated test
    
    Signed-off-by: Ante Jukić <ajukic@nvidia.com>
    
    ---------
    
    Signed-off-by: Ante Jukić <ajukic@nvidia.com>
    
    upgrade to latest mcore and TE (#7908)
    
    * reimport module
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    
    * update mcore and TE commits
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    
    ---------
    
    Signed-off-by: dimapihtar <dpihtar@gmail.com>
    
    Tar codec (#7867)
    
    added missing torch import (#7913)
    
    Signed-off-by: David Mosallanezhad <dmosallanezh@nvidia.com>
    
    add cpu init check (#7889)
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    Fix pinned triton version (#7925)
    
    * Fix pinned triton version
    
    Signed-off-by: Cheng-Ping Hsieh <chsieh@nvidia.com>
    
    * Remove comment
    
    Signed-off-by: Cheng-Ping Hsieh <chsieh@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Change README
    
    Signed-off-by: Cheng-Ping Hsieh <chsieh@nvidia.com>
    
    * Remove flash-attn in Dockerfile
    
    Signed-off-by: Cheng-Ping Hsieh <chsieh@nvidia.com>
    
    * Revert
    
    Signed-off-by: Cheng-Ping Hsieh <chsieh@nvidia.com>
    
    ---------
    
    Signed-off-by: Cheng-Ping Hsieh <chsieh@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    
    fix tp_overlap config var name (#7928)
    
    Signed-off-by: Xiaowei Ren <xren@nvidia.com>
    
    add Dutch P&C FC model info (#7892)
    
    * add Dutch P&C FC model info
    
    Signed-off-by: zhehuaichen <dian.chenzhehuai@gmail.com>
    
    * update order of the results
    
    Signed-off-by: zhehuaichen <dian.chenzhehuai@gmail.com>
    
    ---------
    
    Signed-off-by: zhehuaichen <dian.chenzhehuai@gmail.com>
    
    fix issues with convert_nemo_llama_to_hf.py (#7922)
    
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    fix collate_fn bug for TP > 1
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    make packed dataset work
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    fix nan bug
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    support answer only loss
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    account for padding in cu_seqlens during dataloading for attn kernel
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    fix path for answer_only_loss = false
    
    Signed-off-by: Chen Cui <chcui@nvidia.com>
    
    Modify GPTSFTPackedDataset to respond to pad_to_max_length setting
    
    Signed-off-by: Valerie Sarge <vsarge@nvidia.com>

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -76,7 +76,7 @@ except (ImportError, ModuleNotFoundError):
 try:
     from megatron.core import InferenceParams, parallel_state
     from megatron.core.models.gpt import GPTModel as MCoreGPTModel
-    from megatron.core.models.gpt.gpt_layer_specs import gpt_layer_with_transformer_engine_spec
+    from megatron.core.models.gpt.gpt_layer_specs import get_gpt_layer_with_transformer_engine_spec
     from megatron.core.pipeline_parallel.schedules import get_forward_backward_func
     from megatron.core.transformer.module import Float16Module as MCoreFloat16Module
     from megatron.core.transformer.transformer_config import TransformerConfig
@@ -246,7 +246,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
 
         if self.megatron_amp_O2:
 
-            if not self.with_distributed_adam:
+            if not self.with_distributed_adam and not self.cfg.get("use_cpu_initialization", False):
                 # Pre-allocate the model on GPU to have master parameters allocated on the same device with matching data type
                 if isinstance(self.model, list):
                     for module in self.model:
@@ -308,7 +308,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
         if self.mcore_gpt:
             model = MCoreGPTModel(
                 config=self.transformer_config,
-                transformer_layer_spec=gpt_layer_with_transformer_engine_spec,
+                transformer_layer_spec=get_gpt_layer_with_transformer_engine_spec(),
                 vocab_size=self.cfg.get('override_vocab_size', self.padded_vocab_size),
                 max_sequence_length=self.cfg.get('encoder_seq_length', 512),
                 pre_process=pre_process,
@@ -835,6 +835,8 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
                 required_keys.update(batch.keys())
             else:
                 required_keys.add('attention_mask')
+                if 'cu_seqlens' in batch:
+                    required_keys.add('cu_seqlens')
                 if parallel_state.is_pipeline_first_stage():
                     required_keys.update(('tokens', 'position_ids'))
                 if parallel_state.is_pipeline_last_stage():
@@ -859,6 +861,15 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
             else:
                 # TODO: @eharper can we add this to mcore?
                 forward_args.pop('loss_mask')
+
+                if 'cu_seqlens' in batch:  # packed sequence from GPTSFTPackedDataset
+                    # these args are passed eventually into TEDotProductAttention.forward()
+                    cu_seqlens = batch['cu_seqlens'].squeeze()  # remove batch size dimension (mbs=1)
+                    cu_seqlens = cu_seqlens[: torch.argmin(cu_seqlens)]  # remove -1 "paddings" added in collate_fn
+                    forward_args['cu_seqlens_q'] = cu_seqlens
+                    forward_args['cu_seqlens_kv'] = cu_seqlens
+                    forward_args['qkv_format'] = 'thd'
+
             output_tensor = model(**forward_args)
 
             def loss_func(output_tensor):
@@ -1585,7 +1596,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
             'recompute_method': recompute_method,
             'recompute_num_layers': recompute_num_layers,
             'distribute_saved_activations': False,  # not currently used in NeMo
-            'ub_tp_comm_overlap': ub_tp_comm_overlap,
+            'tp_comm_overlap': ub_tp_comm_overlap,
             'fp8': fp8,
         }
 
